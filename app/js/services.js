f62a8e651d1da90f70c76b5cd55bb19f449e3e89
wytrych <fuzzzym@gmail.com>
2015-02-26 18:22:00 +0000

    More cleanups in controller

--- app/js/services.js
+++ app/js/services.js
@@ -3,11 +3,41 @@ var sequenceMatcherService = angular.module('sequenceMatcherService', []);
 var colorService = angular.module('colorService', []);
 var sequenceDialogService = angular.module('sequenceDialogService', ['colorService']);
 var backendService = angular.module('backendService', ['ngResource']);
+var dataCollectionService = angular.module('dataCollectionService',['sequenceMatcherService','MinIONAppFilters','dataSupplierService']);
+
+dataCollectionService.factory('DataCollection', ['$interval','transcriberFilter','SequenceMatcher','DataChunk',
+	function($interval, transcriberFilter, SequenceMatcher, DataChunk) {
+		return {
+		  interval: "",
+		  stop:
+			function() {
+				$interval.cancel(this.interval)
+			},
+		  start:
+			function(rate,bufferSize,global,buffer,sequences,weights) {
+				//getBuffer = this.getBuffer
+				this.interval = $interval(function() {
+					global.counter += bufferSize
+					buffer = DataChunk.getBuffer(bufferSize,weights)
+					angular.forEach(sequences,function(value,key) {
+						var d = this[key]
+						d.rate += SequenceMatcher.count(transcriberFilter(d.structure),buffer)
+						d.prob = d.rate*100/global.counter;
+					},sequences)
+				},rate)
+			}
+		}
+	}
+])
 
 dataSupplierService.factory('DataChunk', [
-	function() {
+	function($interval, transcriberFilter, SequenceMatcher) {
 		return {
 		  weights: [],
+		  interval: "",
+		  stop: function() {
+			  $interval.cancel(this.interval)
+		  },
 		  getBuffer:
 			function(bufferSize,weights) {
 				this.weights = weights
