8f85b3bb5efa0d9b310de312bf4cb5bcfdb79271
wytrych <fuzzzym@gmail.com>
2015-03-02 15:10:13 +0000

    More tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -8,14 +8,44 @@ describe('MinIONApp', function() {
 
   browser.get('index.html');
 
+  var counter
   var sequenceList = element.all(by.repeater('sequence in sequences'))
+  var bars = element.all(by.css('g.bars'))
   var dialog
 
-  it('should show 4 sequences', function() {
+
+  it('should display that 0 samples were analysed', function() {
+	 expect(	 
+  	 	element(by.css('.label')).getText().then(function(text) {
+		  return text.split(" ")
+    	 	}).then(function(slices) {
+	   	  return parseInt(slices[2])
+  	 	})
+
+	  ).toEqual(0)
+
+  })
+
+
+  it('should show 4 sequences in the list and 4 bars in the SVG', function() {
 
 	  expect(sequenceList.count()).toBe(4)
+	  expect(bars.count()).toBe(4)
+	  expect(	  
+		  element.all(by.css('g.bars rect')).first().getAttribute('y').then(function(value) { return parseInt(value) })
+		  
+	  ).toEqual(400)
 	  
   })
+
+  it('should show the sequence values to be 0.00000',function() {
+	expect(
+		element.all(by.css(".data")).first().getText().then(function(text) {
+			return text.split(/\s/)[1]
+		})
+	).toEqual('0.00000')
+  })
+
 
   it('should show dialog box after click on sequence', function() {
 
@@ -33,6 +63,7 @@ describe('MinIONApp', function() {
 	  expect(dialog.isPresent()).toBeTruthy()
 	  element(by.css('#delete')).click()
 	  expect(sequenceList.count()).toBe(3)
+	  expect(bars.count()).toBe(3)
 	  browser.sleep(1000)
 	  expect(dialog.isPresent()).toBeFalsy()
 
@@ -68,6 +99,7 @@ describe('MinIONApp', function() {
 	  element(by.css('#ok')).click()
 
 	  expect(sequenceList.count()).toBe(4)
+	  expect(bars.count()).toBe(4)
 
 	  browser.sleep(1000)
 
@@ -77,4 +109,74 @@ describe('MinIONApp', function() {
 
   })
 
+
+  it('should start changing values after pressing Start', function() {
+
+
+	element(by.css('#start')).click()
+
+	browser.sleep(1000)
+
+	element(by.css('#stop')).click()
+
+	expect(counter = element(by.css('.label')).getText().then(function(text) {
+		 return text.split(" ")
+    	}).then(function(slices) {
+	   	 return parseFloat(slices[2])
+  	})).toBeGreaterThan(0)
+
+	expect(	  
+		element.all(by.css('g.bars rect')).first().getAttribute('y').then(function(value) { return parseFloat(value) })
+	).toBeLessThan(400)
+	  
+	expect(
+		element.all(by.css(".data")).first().getText().then(function(text) {
+			return parseFloat(text.split(/\s/)[1])
+		})
+	).toBeGreaterThan(0)
+  })
+
+  it('should start changing values after pressing Start again', function() {
+
+
+	element(by.css('#start')).click()
+
+	browser.sleep(1000)
+
+	element(by.css('#stop')).click()
+
+	expect(element(by.css('.label')).getText().then(function(text) {
+		 return text.split(" ")
+    	}).then(function(slices) {
+	   	 return parseFloat(slices[2])
+  	})).toBeGreaterThan(counter)
+
+  })
+
+  it('should clear the values after pressing Clear', function() {
+
+	element(by.css('#clear')).click()
+
+	expect(element(by.css('.label')).getText().then(function(text) {
+		 return text.split(" ")
+    	}).then(function(slices) {
+	   	 return parseInt(slices[2])
+  	})).toEqual(0)
+
+  	browser.sleep(1000)
+
+	expect(	  
+		element.all(by.css('g.bars rect')).first().getAttribute('y').then(function(value) { return parseInt(value) })
+	).toEqual(400)
+
+	expect(
+		element.all(by.css(".data")).first().getText().then(function(text) {
+			return text.split(/\s/)[1]
+		})
+	).toEqual('0.00000')
+  })
+
+
+
 });
+
